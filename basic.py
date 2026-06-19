2e4113bdb766576cc2b9356473ff1b3a395c2e4d
princisoni534 <princisoni534@gmail.com>
2026-06-19 20:32:58 +0530

    added Python datatypes and class

--- basic.py
+++ basic.py
@@ -1,4 +1,44 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-print("all libraries installed successfully")
+ #integer 
+a=10
+print("integer:",a)
+
+#float
+b=10.5
+print("float:",b)
+
+#String
+c="Princi"
+print("String:",c)
+
+#Boolean
+d=True
+print("Boolean:",d)
+
+#Complex
+e = 2 + 3j
+print("Complex:",e)
+
+#list
+f = [10,20,30]
+print("List:",f)
+
+#tuple
+g = (1,2,3)
+print("Tupple:",g)
+
+#Set
+h = {5,6,7}
+print("Set",h)
+
+#Dictionary
+i = {
+    "name":"Princi",
+    "age":19
+}
+print("Dictionary:",i)
+
+#class
+class MyClass:
+    x=5
+obj=MyClass
+print("Classvalue:",obj.x)
